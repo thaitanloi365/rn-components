94b99dde8a51cc783eeb2282bfe652a36c5aae52
tấn lợi thái <thaitanloi365@gmail.com>
2019-06-08 22:17:03 +0700

    add children props

--- src/index.d.ts
+++ src/index.d.ts
@@ -107,6 +107,7 @@ declare module "rn-components" {
     activeOpacity?: number;
     disabled?: boolean;
     containerStyle?: StyleProp<ViewStyle>;
+    children?: React.ReactChild;
   }
 
   interface TextareaProps extends TextInputExtraProps {
