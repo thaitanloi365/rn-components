ad9776065748ce611f8789b6069c88385fc26df5
tấn lợi thái <thaitanloi365@gmail.com>
2019-07-03 00:48:52 +0700

    change statusbar height

--- src/StatusBar/StatusBar.js
+++ src/StatusBar/StatusBar.js
@@ -3,9 +3,10 @@ import { StatusBar as RNStatusBar, View } from "react-native";
 import Device from "../Utils/Device";
 
 const statusBarHeight = Device.getStatusBarHeight(true);
+const isIOS = Device.isIOS();
 
 const StatusBar = props => {
-  if (Device.isIOS()) {
+  if (isIOS) {
     const { style, barStyle, networkActivityIndicatorVisible, showHideTransition, backgroundColor, animated } = props;
     const height = statusBarHeight;
     return (
@@ -23,7 +24,7 @@ const StatusBar = props => {
   const { barStyle, translucent, backgroundColor, animated, style } = props;
   if (translucent) {
     return (
-      <View style={[{ marginBottom: statusBarHeight }, style]}>
+      <View style={[{ height: statusBarHeight }, style]}>
         <RNStatusBar translucent barStyle={barStyle} backgroundColor={backgroundColor} animated={animated} />
       </View>
     );
